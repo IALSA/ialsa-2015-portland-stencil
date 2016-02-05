6c765de04ec3c83ab2f3b652405d3d05db207260
Andrey Koval <koval.andrey@gmail.com>
2016-02-04 21:10:10 -0800

    explore

--- scripts/mplus/look-at-data.R
+++ scripts/mplus/look-at-data.R
@@ -8,19 +8,36 @@ cat("\f")
 library(dplyr)
 library(magrittr) #Pipes
 
-# ---- preceding_scripts ------------------------------------------------------
-# source("./scripts/data/0-import-raw.R")
-
 # ---- @knitr load_data -------------------------------------------------------
-ds0 <- readRDS("./data/unshared/raw/map/ds0.rds")
+rootPath <- getwd()
+filePath <- paste0(rootPath,"/data/unshared/raw/ds0.rds")
+ds0 <- readRDS(filePath)
 ds <- ds0
 str(ds)
 
 
 ds %>% dplyr::count(study)
+ds <- ds %>% dplyr::filter(study == "MAP ")# keep only MAP
 
-# todo: change factors to characters in the initial MAP configuration
-# keep only MAP
-ds <- ds %>% dplyr::filter(study == "MAP")
+# ---- nl_function -----------------------------------------------------------
+# Create function that inspects names and labels
+names.labels <- function(ds){
+  # ds <- dsDemo
+  nl <- data.frame(matrix(NA, nrow=ncol(ds), ncol=2))
+  names(nl) <- c("name","label")
+  for (i in seq_along(names(ds))){
+    nl[i,"name"] <- names(ds[i])
+    if(is.null(attr(ds[[i]], "label")) ){
+      nl[i,"label"] <- NA}else{
+        nl[i,"label"] <- attr(ds[[i]], "label")
+      }
+  }
+  return(nl)
+}
+(nl <- names.labels(ds))
 
-d <- ds[]
+write.csv(nl, file="./data/unshared/derived/nl_raw.csv")
+write.csv(ds, file="./data/unshared/derived/ds.csv")
+
+str(ds$agreeableness)
+sum(ds$conscientiousness)
