5b24e842a34b7c8c582e16207c6977b8dcce7b2d
Andrey Koval <koval.andrey@gmail.com>
2016-02-08 12:51:39 -0800

    #16 supplied MAP data
    the data itself is not shared, prevented by .gitignore exclusion

--- scripts/mplus/look-at-data.R
+++ scripts/mplus/look-at-data.R
@@ -34,10 +34,51 @@ names.labels <- function(ds){
   }
   return(nl)
 }
+# (nl <- names.labels(ds))
+
+# ---- load_varnames ---------------------------------------------------------
 (nl <- names.labels(ds))
 
 write.csv(nl, file="./data/unshared/derived/nl_raw.csv")
 write.csv(ds, file="./data/unshared/derived/ds.csv")
+# augment the names with classifications. Edit the .csv into a new version
+nl_augmentedPath <- paste0(rootPath,"/data/unshared/derived/nl_augmented.csv")
+
+varnames <- read.csv(nl_augmentedPath, stringsAsFactors = F)
+varnames$X <- NULL
+varnames
+
+dplyr::arrange(varnames, type)
+
+# ----- select_subset ------------------------------------
+# select variables you will need for modeling
+selected_items <- c(
+  "id", # personal identifier
+  "fu_year", # Follow-up year
+  "age_at_visit", #Age at cycle - fractional
+
+  "age_bl", #Age at baseline
+  "htm", # Height(meters)
+  "msex", # Gender
+  "race", # Participant's race
+  "educ", # Years of education
+
+  "cts_bname", # Boston naming - 2014
+  "cts_catflu", # Category fluency - 2014
+  "cts_nccrtd", #  Number comparison - 2014
+
+  "fev", # forced expiratory volume
+  "gait_speed", # Gait Speed - MAP
+  "gripavg" # Extremity strength
+)
+
+d <- ds[ , selected_items]
+table(d$fu_year)
+
+# ---- export_data -------------------------------------
+# At this point we would like to export the data in .dat format
+# to be fed to Mplus for any subsequent modeling
+
 
 str(ds$agreeableness)
 sum(ds$conscientiousness)
