ea135c6aa1c0349f6a744350e7b9945c6f18f689
Andrey Koval <koval.andrey@gmail.com>
2016-04-12 07:18:23 -0700

    added names and labels

--- sandbox/elsa-testing/elsa-testing.R
+++ sandbox/elsa-testing/elsa-testing.R
@@ -10,7 +10,7 @@ library(magrittr) # enables piping : %>%
 # ---- load-sources ------------------------------------------------------------
 # Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
 source("./scripts/common-functions.R") # used in multiple reports
-source("./scripts/graph-presets.R") # fonts, colors, themes
+# source("./scripts/graph-presets.R") # fonts, colors, themes
 
 # Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
 requireNamespace("ggplot2") # graphing
@@ -21,7 +21,7 @@ requireNamespace("testit")# For asserting conditions meet expected patterns.
 # requireNamespace("car") # For it's `recode()` function.
 
 # ---- declare-globals ---------------------------------------------------------
-filePath <- "./data/unshared/raw/ELSA_Portland_New_nomiss_years.dat"
+filePath <- "./data/unshared/raw/ELSA_Portland_New_nomiss_years_variablenames.dat"
 variable_names <- c("idauniq", "wave", "dimar", "hedia01", "hedia02", "hedia03", "hedia04", "hedia05", "hedia06", "hedia07", "hedia08", "hedia09", "hedia10", "heage", "hedib01", "hedib02", "hedib03", "hedib04", "hedib05", "hedib06", "hedib07", "hedib08", "hedib09", "hedib10", "hesmk", "heala", "heacta", "heactb", "heactc", "mmwlka", "mmwlkb", "cflisen", "cfani", "cfmem", "cfpascr", "cflisd", "cfmscr", "cfpbscr", "cfptscr", "cfwhz1", "cfwhz2", "cfwhz3", "cfwhz4", "psceda", "pscedb", "pscedc", "pscedd", "pscede", "pscedf", "pscedg", "pscedh", "edqual", "scfamm", "scsca", "scscc", "intdatm", "intdaty", "iintdtm", "iintdty", "indobyr", "indager", "aethnicr", "heagcr", "heagcry", "hedbwlu", "hedbwas", "hedbwar", "hedbwos", "hedbwca", "hedbwpd", "hedbwps", "hedbwad", "hedbwde", "herosmd", "hemobcs", "hehsm86", "hehsm96", "hedcc", "heaidcr", "iintdatm", "iintdaty", "chesmk", "cfmersp", "cfprmem", "cfspeed", "cfanig", "cfexind", "cfaccur", "cfrecal", "hedimbp", "hediman", "hedimmi", "hedimhf", "hedimhm", "hedimar", "hedimdi", "hedbts", "hedimst", "hedimch", "hediblu", "hedibas", "hedibar", "hedibos", "hedibca", "hedibpd", "hedibps", "hedibad", "hedibde", "cfmeind", "cfind", "hedizm51", "hedizm52", "hediagbp", "hediagan", "hediagmi", "hediaghf", "hediaghm", "hediagar", "hediagdh", "hediagdi", "hediagst", "hediaghc", "hebdialu", "hebdiaas", "hebdiaar", "hebdiaos", "hebdiaca", "hebdiapd", "hebdiaps", "hebdiaad", "hebdiade", "hepdiagl", "hepdiadi", "hepdiamd", "hepdiaca", "cvd7dibt", "sex", "sysval", "diaval", "pulval", "mapval", "mmgsd1", "mmgsn1", "mmgsd2", "mmgsn2", "mmgsd3", "mmgsn3", "chol", "trig", "fglu", "htval", "wtval", "bmival", "bmiobe", "fvc1", "fev1", "pf1", "fvc2", "fev2", "pf2", "fvc3", "fev3", "pf3", "htfvc", "htfev", "htpf", "mmssre", "mmssti", "mmssna", "mmstre", "mmstti", "mmftre2", "mmftti", "mmlore", "mmloti", "mmlsre", "mmlsti", "mmcrre", "mmrrre", "mmrrfti", "mmrrtti", "mmrroc", "vitd", "mmftre", "prfvc", "pcfvc", "prfev", "pcfev", "htpef", "prpef", "pcpef", "bagey", "years", "diab", "cardio", "walkspee")
 length(variable_names)
 # ---- load-data ---------------------------------------------------------------
