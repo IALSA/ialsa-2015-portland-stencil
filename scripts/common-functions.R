78bc78e1586df7dae31b27550b6e20584607d191
Andrey Koval <koval.andrey@gmail.com>
2016-05-18 08:38:14 -0700

    common

--- scripts/common-functions.R
+++ scripts/common-functions.R
@@ -1,5 +1,5 @@
 
-# ---- define_lookup_function -------------------------------------------------
+# ---- lookup-names-labels -------------------------------------------------
 # Create function that inspects names and labels
 names_labels <- function(ds){
   nl <- data.frame(matrix(NA, nrow=ncol(ds), ncol=2))
@@ -16,7 +16,7 @@ names_labels <- function(ds){
 }
 # names_labels(ds=oneFile)
 
-
+# ---- basic-histogram-discrete -----------------------------------
 # function to create discrete histogram. taken from RAnalysisSkeleton
 histogram_discrete <- function(
   d_observed,
@@ -62,6 +62,7 @@ histogram_discrete <- function(
   return( g + theme )
 }
 
+# ---- basic-histogram-continous -----------------------------------
 # function to create continuous histogram. taken from RAnalysisSkeleton
 histogram_continuous <- function(
   d_observed,
@@ -94,3 +95,41 @@ histogram_continuous <- function(
   g <- g + ggplot2::geom_text(data=ds_mid_points, ggplot2::aes_string(x="value", y="top", label="label"), color="tomato", hjust=c(1, 0), parse=TRUE)
   return( g )
 }
+
+# ----- basic-line-graph ------------------------------------------------
+basic_line <- function(
+  d_observed,
+  variable_name,
+  time_metric,
+  color_name="black",
+  line_alpha=1,
+  line_size =.5,
+  smoothed = FALSE,
+  main_title     = variable_name,
+  x_title        = paste0("Time metric: ", time_metric),
+  y_title        = variable_name,
+  rounded_digits = 0L
+) {
+
+  d_observed <- as.data.frame(d_observed) #Hack so dplyr datasets don't mess up things
+  d_observed <- d_observed[!base::is.na(d_observed[, variable_name]), ]
+
+  g <- ggplot(d_observed, aes_string(x=time_metric, y = variable_name))
+  if(!smoothed){
+    g <- g + geom_line(aes_string(group="id"), size=line_size, color=scales::alpha(color_name,line_alpha), na.rm=T)
+  } else{
+    g <- g + geom_smooth(aes_string(group="id"),size=line_size,  method="lm",color=scales::alpha(color_name,line_alpha), na.rm=T, se=F )
+    g <- g + geom_smooth(method="loess", color="blue", size=1, fill="gray80", alpha=.3, na.rm=T)
+
+  }
+
+  g <- g + scale_x_continuous(labels=scales::comma_format()) +
+    scale_y_continuous(labels=scales::comma_format()) +
+    # labs(title=main_title, x=x_title, y=y_title) +
+    theme_light() +
+    theme(axis.ticks.length = grid::unit(0, "cm"))
+  return( g )
+}
+
+# g <- basic_line(d, "cogn_global", "fu_year", "salmon", .9, .1, T)
+# g
