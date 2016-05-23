7fcedac1bf7b04f3f14b918c6ee8404d42838b3e
Andrey Koval <koval.andrey@gmail.com>
2016-05-23 14:53:13 -0700

    after conflict resolved

--- sandbox/elsa-testing/elsa-testing.R
+++ sandbox/elsa-testing/elsa-testing.R
@@ -1,4 +1,4 @@
-install.packages(dplyr)
+
 # knitr::stitch_rmd(script="./___/___.R", output="./___/___/___.md")
 #These first few lines run only when the file is run in RStudio, !!NOT when an Rmd/Rnw file calls it!!
 rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
@@ -39,24 +39,27 @@ meta <- dto$metaData
 colnames(dto$unitData)
 unitdata<-dto$unitData
 
-ds <- dto$unitData %>%
+ds0 <- dto$unitData %>%
   dplyr::select_(.dots = dto$metaData$name[!is.na(dto$metaData$retain)])
 
 
-colnames(ds) <- plyr::mapvalues(
-  x      = colnames(ds),
+colnames(ds0) <- plyr::mapvalues(
+  x      = colnames(ds0),
   from   = dto$metaData$name[!is.na(dto$metaData$retain)],
   to     = dto$metaData$varname[!is.na(dto$metaData$retain)]
 )
 
-testit::assert("`diabetes` should be either 1, 0, or NA.", all(is.na(ds$diabetes) | (ds$diabetes %in% c(0, 1))))
-testit::assert("`sex` should be either 'MALE', 'FEMALE', or NA.", all(is.na(ds$sex) | (ds$sex %in% c("MALE", "FEMALE"))))
-testit::assert("`cardio` should be either 1, 0 or NA", all(is.na(ds$cardio)|(ds$cardio %in% c(0,1))))
-testit::assert("`smoke` should be either 'YES', 'NO' or NA.", all(is.na(ds$smoke)|(ds$smoke %in% c("YES", "NO"))))
-testit::assert("`weight_kg'is numeric", is.numeric(ds$weight_kg))
-testit::assert("`weight_kg` is missing or positive", all(is.na(ds$weight_kg) | (ds$weight_kg >= 0)))
-testit::assert("`height_cm` is numneric", is.numeric(ds$height_cm))
-testit::assert("`height_cm` is missing or positive", all(is.na(ds$height_cm) | (ds$height_cm >= 0)))
+testit::assert("`sex` should be either 'MALE', 'FEMALE', or NA.", all(is.na(ds0$sex) | (ds0$sex %in% c("MALE", "FEMALE"))))
+testit::assert("`height_cm`is numeric; it's missing or positive", all(is.numeric(ds0$height_cm) | is.na(ds0$height_cm) | (ds0$height_cm >= 0)))
+testit::assert("`weight_kg'is numeric; it's missing or positive", all(is.numeric(ds0$weight_kg) | is.na(ds0$weight_kg) | (ds0$weight_kg >= 0)))
+
+testit::assert("`diabetes` should be either 1, 0, or NA.", all(is.na(ds0$diabetes) | (ds0$diabetes %in% c(0, 1))))
+testit::assert("`smoke` should be either 'YES', 'NO' or NA.", all(is.na(ds0$smoke)|(ds0$smoke %in% c("YES", "NO"))))
+testit::assert("`cardio` should be either 1, 0 or NA", all(is.na(ds0$cardio)|(ds0$cardio %in% c(0,1))))
+
+ds0 %>%
+  dplyr::select(id,wave,age_bl, years_since_bl) %>%
+  dplyr::slice(1:10)
 
 
 # compute pulmonary variables (max of the three)
@@ -69,10 +72,14 @@ for(i in 1:nrow(ds0)){
 # assemple data for analysis
 ds <- ds0 %>%
   dplyr::mutate(
-    diabetes      = as.logical(diabetes),
+    # design
+    year_bl        = 2002, # year of the first wave
+    year          = as.numeric(year_bl + years_since_bl),
+    age_bl         = as.numeric(age_bl),
+    age           = as.numeric(age_bl + years_since_bl),
+    # covariates
     male          = as.logical(ifelse(!is.na(sex), sex=="MALE", NA_integer_)),
-    #add cardio
-    #add smoke
+    diabetes      = as.logical(diabetes),
     cardio        = as.logical(ifelse(!is.na(cardio), cardio==1, NA_integer_)),
     smoke         = as.logical(ifelse(!is.na(smoke), smoke==1, NA_integer_)),
     # outcomes
@@ -90,7 +97,6 @@ head(ds)
 
 
 # ---- functions-to-examime-temporal-patterns -------------------
-
 view_temporal_pattern <- function(ds, measure, seed_value = 42){
   set.seed(seed_value)
   ds_long <- ds
@@ -100,11 +106,11 @@ view_temporal_pattern <- function(ds, measure, seed_value = 42){
     dplyr::select_("id","wave", measure)
   print(d)
 }
-# ds %>%  view_temporal_pattern("sex", 1)
-
+# ds %>%  view_temporal_pattern("male", 2)
 
 # examine the descriptives over waves
 over_waves <- function(ds, measure_name, exclude_values="") {
+  ds <- as.data.frame(ds)
   testit::assert("No such measure in the dataset", measure_name %in% unique(names(ds)))
   # measure_name = "htval"; wave_name = "wave"; exclude_values = c(-99999, -1)
   cat("Measure : ", measure_name,"\n", sep="")
@@ -127,70 +133,55 @@ over_waves <- function(ds, measure_name, exclude_values="") {
   return(as.data.frame(t))
 
 }
-# ds %>% over_waves("years")
+# ds %>% over_waves("year_bl")
+# ---- examine-temporal-patterns ----------------------------------
 
-# ---- print-temporal-patterns ----------------------------------
-# ds %>% view_temporal_pattern("year_born", 2)
-# ds %>% view_temporal_pattern("age_bl", 2)
-# ds %>% view_temporal_pattern("years_since_bl", 2)
-#
-#
-# # supposed to be time-invariant
-# ds %>% over_waves("sex") # 1, 2, 3, 4, 5, 6
-# ds %>% over_waves("year_born") # 1, 2, 3, 4, 5, 6
-# ds %>% over_waves("edu") # 1, 2, 3, 4, 5, 6
-# ds %>% over_waves("marital") # 1, 2, 3, 4, 5, 6
-#
-#
-# ds %>% over_waves("diabetes") # 1, 2
-# ds %>% over_waves("cardio") # 1, 2
-# ds %>% over_waves("smoke") # 1, 2, 3, 4, 5, 6
-# ds %>% over_waves("alcohol") # 1
-# ds %>% over_waves("height_cm"); # 2, 4, 6
-# ds %>% over_waves("weight_kg") # 2, 4, 6
-#
-#
-# # supposed to be time-variant
-#
-# ds %>% over_waves("yborn") # 1, 2, 3, 4, 5, 6
-# ds %>% view_temporal_pattern("years_since_bl", 5)
-#
-# ds %>% over_waves("word_recall_im") # 1, 2, 3, 4, 5, 6
-# ds %>% over_waves("word_recall_de") # 1, 2, 3, 4, 5, 6
-# ds %>% over_waves("animals") # 1, 2, 3, 4, 5
-#
-# ds %>% over_waves("fvc_1") # 2, 4
-# ds %>% over_waves("fvc_2") # 2, 4
-# ds %>% over_waves("fvc_3") # 2, 4
-#
-# ds %>% over_waves("fev_1") # 2, 4
-# ds %>% over_waves("fev_2") # 2, 4
-# ds %>% over_waves("fev_3") # 2, 4
-#
-#
-# ds %>% over_waves("pef_1") # 2, 4
-# ds %>% over_waves("pef_2") # 2, 4
-# ds %>% over_waves("pef_3") # 2, 4
-#
-# ds %>% over_waves("gait") # 1, 2, 3, 4, 5, 6
-# ds %>% over_waves("grip") # 2, 4, 6
-#
-# ds %>% over_waves("speed") # 3
+ds %>% view_temporal_pattern("year_bl", 2) # year at baseline
+ds %>% view_temporal_pattern("year", 2) # year of measurement
+
+ds %>% view_temporal_pattern("age_bl", 2) # age at baseline
+ds %>% view_temporal_pattern("age", 2) # age at measurement
+
+
+# ---- force-to-static-sex ---------------------------
+ds %>% view_temporal_pattern("male", 2) # sex
+ds %>% over_waves("male") # 1, 2, 3, 4, 5, 6
+# check that values are the same across waves
+ds %>%
+  dplyr::group_by(id) %>%
+  dplyr::summarize(unique = length(unique(male))) %>%
+  dplyr::arrange(desc(unique)) # unique > 1 indicates change over wave
+# grab the value for the first wave and forces it to all waves
+ds <- ds %>%
+  dplyr::group_by(id) %>%
+  dplyr::mutate(
+    male_bl   = dplyr::first(male) # grabs the value for the first wave and forces it to all waves
+  ) %>%
+  dplyr::ungroup()
+# examine the difference
+ds %>% over_waves("male_bl")
+ds %>% view_temporal_pattern("male_bl", 2) # sex
+
+# ds_long <- ds
+# (ids <- 117781)
+# d <- ds_long %>%
+#   dplyr::filter(id %in% ids ) %>%
+#   dplyr::select_("id","wave", "male", "male_bl")
+# d
 
 
 
-# ---- make-new-variables -----------------------
-year_of_baseline <- 2002
-
-ds$fvc <- max(ds$fvc_1, ds$fvc_2, ds$fvc_3)
-ds$fev <- max(ds$fev_1, ds$fev_2, ds$fev_3)
-ds$pef <- max(ds$pef_1, ds$pef_2, ds$pef_3)
-
-ds$age_at_visit <- year_of_baseline - ds$year_born + ds$years_since_bl
-
-# ---- force-time-invariant-values -------------------
-
-# bl_value <- as.character(ds[ds$id == id, "sex"][1])
+# ---- force-to-static-education ---------------------------
+ds %>% view_temporal_pattern("edu", 2) # sex
+ds %>% over_waves("edu") # 1, 2, 3, 4, 5, 6
+# check that values are the same across waves
+ds %>%
+  dplyr::group_by(id) %>%
+  dplyr::summarize(unique = length(unique(edu))) %>%
+  dplyr::arrange(desc(unique)) # unique > 1 indicates change over wave
+# edu is truely time-invariant
+ds <- ds %>% dplyr::mutate( edu_bl = edu)
+ds %>% over_waves("edu_bl")
 
 # ---- force-to-static-height ---------------------------
 ds %>% view_temporal_pattern("height_cm", 2)
@@ -222,61 +213,94 @@ ds %>%
   dplyr::summarize(unique = length(unique(diabetes))) %>%
   dplyr::arrange(desc(unique)) # unique > 1 indicates change over wave
 # grab the value for the first wave and forces it to all waves
-
 ds <- ds %>%
   dplyr::group_by(id) %>%
   dplyr::mutate(
     diabetes_bl   = dplyr::first(diabetes) # grabs the value for the first wave and forces it to all waves
   ) %>%
-  dplyr::ungroup() %>%
-  dplyr::select(
-    -diabetes
-  )
-
-#TODO: create baseline variables for these measures.  Follow the 'diabetes' template above.
-#* sex
-#* edu
-#* marital
-#* cardio
-#* smoke
-#* alcohol
-#* height_cm
+  dplyr::ungroup()
+# examine the difference
+ds %>% over_waves("diabetes_bl")
 
 
-ds_long <- ds
-(ids <- 117781)
-d <- ds_long %>%
-  dplyr::filter(id %in% ids ) %>%
-  dplyr::select_("id","wave", "diabetes_bl")
-d
-
-
-names(ds)
-
-ds2 <- ds %>%
-  dplyr::select(id, wave,
-                sex, edu, marital, diabetes_bl,
-                fvc, fev, pef,
-                word_recall_im, word_recall_de, animals)
-
-head(ds2)
-
-#Long to wide
+# ---- force-to-static-cardio ---------------------------
+ds %>% view_temporal_pattern("cardio", 2)
+ds %>% over_waves("cardio") # 1, 2
+# check that values are the same across waves
 ds %>%
-  dplyr::select(id, wave, animals, word_recall_de ) %>%  #
-  tidyr::spread_(key="wave", value=c("animals", "word_recall_de"))
+  dplyr::group_by(id) %>%
+  dplyr::summarize(unique = length(unique(cardio))) %>%
+  dplyr::arrange(desc(unique)) # unique > 1 indicates change over wave
+# grab the value for the first wave and forces it to all waves
+ds <- ds %>%
+  dplyr::group_by(id) %>%
+  dplyr::mutate(
+    cardio_bl   = dplyr::first(cardio) # grabs the value for the first wave and forces it to all waves
+  ) %>%
+  dplyr::ungroup()
+# examine the difference
+ds %>% over_waves("cardio_bl")
 
-# variables_static <- c("id", "sex", "edu", "marital", "diabetes_bl")
-variables_static <- c("id", "diabetes_bl")
-variables_longitudinal <- c(
-  "smoke", "alcohol", "word_recall_im", "animals", "word_recall_de",
-  "year_born", "speed", "height_cm", "weight_kg", "fvc_1", "fev_1",
-  "pef_1", "fvc_2", "fev_2", "pef_2", "fvc_3", "fev_3", "pef_3",
-  "age_bl", "years_since_bl", "cardio", "gait", "grip", "male",
-  "fvc", "fev", "pef", "age_at_visit"
-)
 
-ds_wide <- ds %>%
+# ---- force-to-static-smoke ---------------------------
+ds %>% view_temporal_pattern("smoke", 2)
+ds %>% over_waves("smoke") # 1, 2, 3, 4, 5, 6
+# check that values are the same across waves
+ds %>%
+  dplyr::group_by(id) %>%
+  dplyr::summarize(unique = length(unique(smoke))) %>%
+  dplyr::arrange(desc(unique)) # unique > 1 indicates change over wave
+# grab the value for the first wave and forces it to all waves
+ds <- ds %>%
+  dplyr::group_by(id) %>%
+  dplyr::mutate(
+    smoke_bl   = dplyr::first(smoke) # grabs the value for the first wave and forces it to all waves
+  ) %>%
+  dplyr::ungroup()
+# examine the difference
+ds %>% over_waves("smoke_bl")
+
+
+# ---- prepare-for-mplus -------------------------------
+names(ds)
+ds2 <- ds %>%
+  dplyr::mutate(
+    id             = as.numeric(id),
+
+    male_bl        = as.numeric(male_bl),
+    diabetes_bl    = as.numeric(diabetes_bl),
+    cardio_bl      = as.numeric(cardio_bl),
+    smoke_bl       = as.numeric(smoke_bl),
+
+    edu_bl         = as.numeric(edu_bl),
+    word_recall_im = as.numeric(word_recall_im),
+    word_recall_de = as.numeric(word_recall_de),
+    animals        = as.numeric(animals)
+  ) %>%
+  dplyr::select(id, wave, year_bl, year, age_bl,  age,
+                male_bl, edu_bl, height_cm_bl, diabetes_bl, cardio_bl, smoke_bl,
+                fev, fvc, pef,
+                word_recall_im, word_recall_de, animals)
+str(ds2)
+
+
+
+# ---- force-time-invariant-values -------------------
+# ds_long <- ds2
+# (ids <- 117781)
+# d <- ds_long %>%
+#   dplyr::filter(id %in% ids ) %>%
+#   dplyr::select_("id","wave", "male")
+# d
+
+
+variables_static <- c("id", "year_bl", "age_bl", "male_bl", "edu_bl",
+                      "height_cm_bl", "diabetes_bl", "cardio_bl", "smoke_bl")
+variables_longitudinal <- setdiff(colnames(ds2),variables_static)
+(variables_longitudinal <- variables_longitudinal[!variables_longitudinal=="wave"])
+
+
+ds_wide <- ds2 %>%
   # dplyr::select(id, wave, animals, word_recall_de ) %>%
   # gather(variable, value, -(id:wave)) %>%
   dplyr::select_(.dots=c(variables_static, "wave", variables_longitudinal)) %>%
@@ -285,18 +309,8 @@ ds_wide <- ds %>%
   tidyr::unite(temp, variable, wave) %>%
   tidyr::spread(temp, value)
 
-# readr::write_csv(ds_wide, path="zzzz")
-
-# ---- tweak-data --------------------------------------------------------------
-ds %>% view_temporal_pattern("height_cm", 2)
-ds %>% view_temporal_pattern("diabetes", 2)
-ds %>% view_temporal_pattern("smoke", 7)
-
-# ---- basic-table --------------------------------------------------------------
-ds %>%
-  dplyr::group_by(wave) %>%
-  dplyr::summarize(count = n())
 
 
-# ---- basic-graph --------------------------------------------------------------
+readr::write_csv(ds_wide, path="./data/unshared/derived/esla-mplus-data.dat")
+write(names(ds_wide), "./data/unshared/derived/esla-mplus-varnames.txt", sep=" ")
 
