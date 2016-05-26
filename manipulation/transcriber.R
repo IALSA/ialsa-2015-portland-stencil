489faa7964e591ce0d1f91983ca1903270bc9bcb
Will Beasley <wibeasley@hotmail.com>
2016-05-25 18:38:56 -0700

    skeleton of transcriber

--- manipulation/transcriber.R
+++ manipulation/transcriber.R
@@ -1,1 +1,82 @@
 # The function of this report is to print the input file to be used in estimation.
+
+
+# knitr::stitch_rmd(script="./manipulation/te-ellis.R", output="./manipulation/stitched-output/te-ellis.md")
+# For a brief description of this file see the presentation at
+#   - slides: https://rawgit.com/wibeasley/RAnalysisSkeleton/master/documentation/time-and-effort-synthesis.html#/
+#   - code: https://github.com/wibeasley/RAnalysisSkeleton/blob/master/documentation/time-and-effort-synthesis.Rpres
+rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
+
+# ---- load-sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+source("./manipulation/translator.R") #Loads `mplus_generator_bivariate()`
+
+# ---- load-packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+library(RODBC, quietly=TRUE)
+library(magrittr, quietly=TRUE)
+
+# Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("REDCapR") #devtools::install_github(repo="OuhscBbmc/REDCapR")
+requireNamespace("OuhscMunge") #devtools::install_github(repo="OuhscBbmc/OuhscMunge")
+
+requireNamespace("readr")
+requireNamespace("tidyr")
+requireNamespace("dplyr") #Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
+requireNamespace("testit") #For asserting conditions meet expected patterns.
+# requireNamespace("car") #For it's `recode()` function.
+
+# ---- declare-globals ---------------------------------------------------------
+path_credential_catalog <- "./data/unshared/security/phi-free.credentials"
+path_prototype          <- "./manipulation/translator-support/prototype-wide.inp"
+
+survey_ids_to_retain <- c(5L)
+
+desired_fields <- c("record_id", "mplus_syntax") #Get other stuff like path_syntax
+
+#Factors from REDCap
+# investigations_to_run <- c("ELSA")
+# investigation_labels  <- c("EAS", "ELSA", "HRS", "ILSE", "LASA", "MAP", "NUAGE", "OCTO", "SATSA")
+
+# ---- load-data ---------------------------------------------------------------
+
+# Read the credentials
+credential_catalog <- REDCapR::retrieve_credential_local(path_credential_catalog, project_id=447) #For the catalog
+
+# Retrieve from the PCS (pre-conference survey)
+ds_catalog <- REDCapR::redcap_read(
+  redcap_uri=credential_catalog$redcap_uri,
+  token=credential_catalog$token,
+  fields = desired_fields
+  )$data
+
+
+rm(path_credential_catalog)
+
+# ---- tweak-data --------------------------------------------------------------
+# OuhscMunge::column_rename_headstart(ds_pcs) #Spit out columns to help write call ato `dplyr::rename()`.
+ds <- ds_catalog %>%
+  dplyr::rename_(
+  ) %>%
+  dplyr::mutate(
+    # investigation                                            = factor(investigation, levels=investigation_levels, labels=investigation_labels)
+  ) %>%
+  dplyr::filter(
+    # survey_id      %in%  survey_ids_to_retain
+  )
+
+
+
+# ---- save-syntax-to-file -----------------------------------------------------------
+
+
+# ---- verify-values -----------------------------------------------------------
+
+testit::assert("All model syntax should be at least 500 characters.", all(nchar(ds$mplus_syntax) >= 500L))
+
+
+# ---- specify-columns-to-upload -----------------------------------------------
+# Nothing to upload
+
+# ---- upload-to-db ------------------------------------------------------------
+# Nothing to upload
